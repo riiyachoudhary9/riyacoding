dc5e9640aab058992e558e82c819371d0a6cc6d3
Riya Choudhary <riiyachoudhary9@gmail.com>
2025-11-19 19:13:33 +0530

    Add files via upload

---
+++ for.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+
+int main() {
+        int i;
+        int a[9];
+        printf ("element of array:\n" );
+        scanf("%d",&a[i]);
+        for(i=8;i<=0;i--)
+        printf("%d",a[i]);
+
+    return 0;
+}
