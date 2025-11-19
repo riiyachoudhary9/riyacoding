b23df51ec6ca467f098c12c522ff7472dcc4dfb8
Riya Choudhary <riiyachoudhary9@gmail.com>
2025-11-19 19:47:51 +0530

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,16 @@
+
+// 2. Operators example: arithmetic, relational, logical
+#include <stdio.h>
+int main() {
+    int x = 10, y = 3;
+    int sum = x + y;
+    int rel = (x > y);
+    int logic = (x > 5 && y < 5);
+
+    printf("Sum: %d\n", sum);
+    printf("x > y: %d\n", rel);
+    printf("Logical AND result: %d\n", logic);
+    return 0;
+}
+
+
